554bea8d333222078d0e57c55bb4eb21b1d931dd
Suraj Roy <surajroy7430@gmailcom>
2023-07-31 21:30:26 +0530

    3

--- src/components/App.js
+++ src/components/App.js
@@ -13,20 +13,24 @@ const App = () => {
     };
 
     useEffect(() => {
-        if(id !== ''){
-            setLoader(true);
-        }
-        try {
-            const fetchData = async () => {
-                const response = await fetch('https://jsonplaceholder.typicode.com/photos/${id}');
-                const data = await response.json();
-                setPhotoData(data);
-                setLoader(false);
+        const fetchData = async () => {
+            if(id !== ''){
+                setLoader(true);
+                try {
+                    const response = await fetch('https://jsonplaceholder.typicode.com/photos/${id}');
+                    const data = await response.json();
+                    setPhotoData(data);
+                    setLoader(false);
+                }
+                catch (error) {
+                    console.log('Error: ' + error);
+                    setLoader(false);
+                    setPhotoData(null);
+                }
             }
-        } catch (error) {
-            console.log('Error: ' + error);
-            setLoader(false);
-            setPhotoData(null);
+            else{
+                setPhotoData(null);
+            }
         }
 
         fetchData();
