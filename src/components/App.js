92e6d3ac68a7996b67eecb5a8cf0923f419bc8d2
Suraj Roy <surajroy7430@gmail.com>
2023-07-31 18:16:30 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
+
 const App = () => {
-  
+    const [id, setId] = useState('');
+    const [photoData, setPhotoData] = useState(null);
+    const [loader, setLoader] = useState(false);
+
+    const handleInputChange = (e) => {
+        setId(e.target.value);
+    };
+
+    useEffect(() => {
+        setLoader(true);
+        try {
+            const fetchData = async () => {
+                const response = await fetch('https://jsonplaceholder.typicode.com/photos/${id}');
+                const data = await response.json();
+                setPhotoData(data);
+                setLoader(false);
+            }
+        } catch (error) {
+            console.log('Error: ' + error);
+            setLoader(false);
+            setPhotoData(null);
+        }
+
+        if(id !== ''){
+            fetchData();
+        }
+    }, [id]);
+
+    return (
+        <div>
+            <input 
+                type="number"
+                value={id}
+                onChange={handleInputChange}
+            />
+            {loader ? <Loader /> : null}
+            {photoData && <PhotoFrame url={photoData.url} title={photoData.title} />}
+        </div>
+    )
 }
 
 
