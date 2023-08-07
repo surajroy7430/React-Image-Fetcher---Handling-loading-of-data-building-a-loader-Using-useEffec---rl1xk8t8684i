a0ffb9ee9f37da5a739caadc366889ed4973623a
Suraj Roy <surajroy7430@gmailcom>
2023-08-07 21:57:12 +0530

    fetch changed

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
             if(id !== ''){
                 setLoader(true);
                 try {
-                    const response = await fetch('https://jsonplaceholder.typicode.com/photos/${id}');
+                    const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
                     const data = await response.json();
                     setPhotoData(data);
                     setLoader(false);
